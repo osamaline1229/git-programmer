8e108a21bde0b02cb9f3c0d1865f2578a6d184fb
unknown <osama98138139@gmail.com>
2023-05-29 05:28:35 -0700

    my first code

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int x = 4;
+        int y = x + 5;
+        int u = x + y;
+        Console.WriteLine(x);
+        Console.WriteLine(y);
+        Console.WriteLine(u);
+        int o = Math.Max(u, y);
+        Console.WriteLine("the biigger value:" + o);
+        int p = o ^ 2;
+        Console.WriteLine("square:" + p );
+    }
+}
