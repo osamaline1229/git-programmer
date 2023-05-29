27f0fcaba3f9cf125d96a5f269da8b775cba36e5
unknown <osama98138139@gmail.com>
2023-05-29 05:35:33 -0700

    second code

---
+++ second_code.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("write your name:");
+        string myna = Console.ReadLine();
+        Console.WriteLine("write your gba:");
+        int mygba = Convert.ToInt32(Console.ReadLine());
+
+        int c = mygba / 2;
+
+        
+
+        Console.WriteLine("my name is:" + myna);
+        Console.WriteLine("my gba is:" + mygba);
+        Console.WriteLine("your divided gba:"+ c);
+
+    }
+}
